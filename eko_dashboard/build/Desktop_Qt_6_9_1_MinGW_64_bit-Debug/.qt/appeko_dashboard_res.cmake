f0d1e7c449541aa30d6abd9f7bd85ee668f90ffe
Igor <igor@Thinkpad.T440P>
2025-08-29 15:51:05 +0200

    Czyszczene starych plikow

--- eko_dashboard/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/appeko_dashboard_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appeko_dashboard")
-set(working_dir "C:/Users/igorl/Desktop/Ekoenergia/dashboard/eko_dashboard")
-set(src_and_dest_list
-
-)
-set(timestamp_file "C:/Users/igorl/Desktop/Ekoenergia/dashboard/eko_dashboard/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/appeko_dashboard_res.txt")
